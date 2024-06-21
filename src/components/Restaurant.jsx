eb5c1982d74396c99f7e0d94cdbef522553d3618
Prabhatlodhi <lodhi.prabhat@gmail.com>
2024-06-21 07:18:50 +0530

    card image updated

--- src/components/Restaurant.jsx
+++ src/components/Restaurant.jsx
@@ -1,25 +1,24 @@
 import { useValue } from "../context/ContextProvider";
 import "../assets/css/components/Restaurant.css";
 import { useNavigate } from "react-router-dom";
-
 import StarBorderIcon from "@mui/icons-material/StarBorder";
 
 const Restaurant = () => {
   const navigate = useNavigate();
   const {
-    state: { cardData },dispatch
+    state: { cardData }, dispatch
   } = useValue();
 
-  const detailsPage = (restaurantId)=>{
+  const detailsPage = (restaurantId) => {
     console.log(restaurantId, "restaurantId");
-    dispatch({type:"RESTAURANT_ID", payload:restaurantId})
+    dispatch({ type: "RESTAURANT_ID", payload: restaurantId });
     navigate("/detailsPage");
   }
 
   return (
     <div className="restaurant-container">
       {cardData?.map((item, index) => (
-        <div className="restaurant-card" key={index} onClick={()=>detailsPage(item?.info.id)} >
+        <div className="restaurant-card" key={index} onClick={() => detailsPage(item?.info.id)} >
           <img
             src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/${item?.info?.cloudinaryImageId}`}
             alt=""
@@ -27,10 +26,9 @@ const Restaurant = () => {
           />
           <div>
             <p> {item?.info?.name} </p>
-            <p className="restaurant-rating" > <StarBorderIcon/>  {item?.info.avgRatingString} </p>
-            <p  className="restaurant-cuisines" >{item?.info.cuisines.toString()} </p>
-            <p>  {item?.info.locality} </p>
-        
+            <p className="restaurant-rating"> <StarBorderIcon /> {item?.info.avgRatingString} </p>
+            <p className="restaurant-cuisines">{item?.info.cuisines.toString()} </p>
+            <p> {item?.info.locality} </p>
           </div>
         </div>
       ))}
